7220e2c6fedd48ed3c8535cf2e14090eb5897fe9
furahayves <yvesderricks@gmail.com>
2026-03-12 08:58:41 +0200

    initial commit: Minimalist reactjs app

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Link, Route, Router, Routes } from "react-router-dom";
+import AboutPage from "./components/AboutPage.jsx";
+import ContactPage from "./components/ContactPage.jsx";
+import HomePage from "./components/HomePage.jsx";
+
+const App = () => {
+	return (
+		<div>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+				<li>
+					<Link to="/contact">Contact</Link>
+				</li>
+			</ul>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/about" element={<AboutPage />} />
+				<Route path="/contact" element={<ContactPage />} />
+			</Routes>
+		</div>
+	);
+};
+
+export default App;
